cce382e3cbe07e6e5a98bbca3bd4c6ad62a0e3ac
crishuinca <cr.huinca@duouc.cl>
2026-09-12 03:27:16 -0300

    a produccion xavales

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  msalClientId: '7dca2eec-affd-406c-bfe9-53f2a3635f96',
+  msalAuthority:
+    'https://login.microsoftonline.com/f0ec7247-e735-4fe5-9376-8bd06cc41a58',
+  redirectUri: 'http://localhost:4200',
+  apiBaseUrl: 'http://18.232.173.60:8080/api',
+  apiScopes: [
+    'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Read',
+    'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Write',
+  ],
+};
