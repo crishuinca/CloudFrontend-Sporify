b48c8b8f16ea56442597baddd58da59c7925e9da
crishuinca <cr.huinca@duouc.cl>
2026-09-12 00:15:51 -0300

    configuracion SPA, con Azure y cortesito base

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  msalClientId: '7dca2eec-affd-406c-bfe9-53f2a3635f96',
+  msalAuthority: 'https://login.microsoftonline.com/tobal.onmicrosoft.com',
+  redirectUri: 'http://localhost:4200',
+  apiBaseUrl: 'http://localhost:8080/api',
+  apiScopes: [
+    'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Read',
+    'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Write',
+  ],
+};
