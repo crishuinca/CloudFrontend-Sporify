9d39d4fc8456453596cd1530c5eb12b1a323e0c7
crishuinca <cr.huinca@duouc.cl>
2026-09-13 13:19:46 -0300

    API hacia el HTTP Api Gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   msalAuthority:
     'https://login.microsoftonline.com/f0ec7247-e735-4fe5-9376-8bd06cc41a58',
   redirectUri: 'http://localhost:4200',
-  apiBaseUrl: 'http://18.232.173.60:8080/api',
+  apiBaseUrl: 'https://c1hak6kalj.execute-api.us-east-1.amazonaws.com/api',
   apiScopes: [
     'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Read',
     'api://0403d276-8c91-4a42-9f38-24f168ccc869/Music.Write',
